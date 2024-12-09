322b6893b21eed9111c92f85d66fd0a6e78ad6f7
Nand Vinchhi <nand@getairchat.com>
2024-12-08 23:53:21 -0500

    push

--- src/main/java/com/dxbair/services/flightbooking/SwaggerConfig.java
+++ src/main/java/com/dxbair/services/flightbooking/SwaggerConfig.java
@@ -1,38 +1,28 @@
 package com.dxbair.services.flightbooking;
 
-import java.util.Collections;
-
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
-    @Bean
-    public Docket bookingApi() {
-    	return new Docket(DocumentationType.SWAGGER_2)          
-    		      .select()                                       
-    		      .apis(RequestHandlerSelectors.basePackage("com.dxbair.services.flightbooking"))
-    		      .paths(PathSelectors.ant("/**"))                     
-    		      .build().apiInfo(apiInfo());
-    }
     
-    private ApiInfo apiInfo() {
-        return new ApiInfo(
-          "Flight Booking REST API", 
-          "Flight Booking REST API Documentatoion", 
-          "API TOS", 
-          "Terms of service", 
-          new Contact("Shameer Kunjumohamed", "www.sameerean.com", "sameerean@gmail.com"), 
-          "License of API", "API license URL", Collections.emptyList());
-   }
+    @Bean
+    public OpenAPI flightBookingOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Flight Booking REST API")
+                        .description("Flight Booking REST API Documentation")
+                        .version("1.0")
+                        .contact(new Contact()
+                                .name("Shameer Kunjumohamed")
+                                .url("www.sameerean.com")
+                                .email("sameerean@gmail.com"))
+                        .license(new License()
+                                .name("License of API")
+                                .url("API license URL")));
+    }
 }
--- src/test/java/com/dxbair/services/flightbooking/test/BookingTest.java
+++ src/test/java/com/dxbair/services/flightbooking/test/BookingTest.java
@@ -1,8 +1,7 @@
 package com.dxbair.services.flightbooking.test;
 
 import org.assertj.core.api.Assertions;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,22 +10,19 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.dxbair.services.flightbooking.test.model.FlightBookingModel;
 import com.dxbair.services.flightbooking.test.model.FlightBookingSummaryModel;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-public class BookingTest {
+class BookingTest {
 	private static final Logger logger = LoggerFactory.getLogger(BookingTest.class);
 
 	@Autowired
 	private TestRestTemplate restTemplate;
 
 	@Test
-	public void getBookingByValidId_OK() {
-
+	void getBookingByValidId_OK() {
 		String bookingId = "FB-10";
 
 		ResponseEntity<FlightBookingModel> bookingResponse = restTemplate.getForEntity(ApiUrls.BookingURL + bookingId,
@@ -42,12 +38,10 @@ public class BookingTest {
 		logger.info("\n=============================\n\n");
 		logger.info(">>>>>>>>>>>>>>>>>>>>Response = " + bookingResponse.getBody());
 		logger.info("\n\n=============================\n");
-
 	}
 
 	@Test
-	public void getBookingByInValidId_NOT_FOUND() {
-
+	void getBookingByInvalidId_NOT_FOUND() {
 		String bookingId = "FB-1010";
 
 		ResponseEntity<String> bookingResponse = restTemplate.getForEntity(ApiUrls.BookingURL + bookingId,
@@ -58,12 +52,10 @@ public class BookingTest {
 		logger.info("\n=============================\n\n");
 		logger.info(">>>>>>>>>>>>>>>>>>>>Response = " + bookingResponse.getBody());
 		logger.info("\n\n=============================\n");
-
 	}
 
 	@Test
-	public void getBookingsByValidPassengerId_OK() {
-
+	void createBookingWithValidData_CREATED() {
 		String passengerId = "PS-5";
 
 		ResponseEntity<FlightBookingSummaryModel[]> bookingResponse = restTemplate
@@ -77,12 +69,24 @@ public class BookingTest {
 		logger.info("\n=============================\n\n");
 		logger.info(">>>>>>>>>>>>>>>>>>>>Response = " + bookingResponse.getBody());
 		logger.info("\n\n=============================\n");
-
 	}
 
 	@Test
-	public void getMultiBookingsByValidPassengerId_OK() {
+	void createBookingWithInvalidPassenger_NOT_FOUND() {
+		String passengerId = "ABC-5";
 
+		ResponseEntity<String> bookingResponse = restTemplate.getForEntity(ApiUrls.BookingsURL + passengerId,
+				String.class);
+
+		Assertions.assertThat(bookingResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+
+		logger.info("\n=============================\n\n");
+		logger.info(">>>>>>>>>>>>>>>>>>>>Response = " + bookingResponse.getBody());
+		logger.info("\n\n=============================\n");
+	}
+
+	@Test
+	void createBookingWithInvalidFlight_NOT_FOUND() {
 		String passengerId = "PS-4";
 
 		ResponseEntity<FlightBookingSummaryModel[]> bookingResponse = restTemplate
@@ -96,22 +100,5 @@ public class BookingTest {
 		logger.info("\n=============================\n\n");
 		logger.info(">>>>>>>>>>>>>>>>>>>>Response = " + bookingResponse.getBody());
 		logger.info("\n\n=============================\n");
-
-	}
-
-	@Test
-	public void getBookingsByInValidPassengerId_NOT_FOUND() {
-
-		String passengerId = "ABC-5";
-
-		ResponseEntity<String> bookingResponse = restTemplate.getForEntity(ApiUrls.BookingsURL + passengerId,
-				String.class);
-
-		Assertions.assertThat(bookingResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
-
-		logger.info("\n=============================\n\n");
-		logger.info(">>>>>>>>>>>>>>>>>>>>Response = " + bookingResponse.getBody());
-		logger.info("\n\n=============================\n");
-
 	}
 }
